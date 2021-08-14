494d9e3666c5ea9fdf08ca59e979163cea12e365
KAVISH072010 <86460565+KAVISH072010@users.noreply.github.com>
2021-08-14 14:23:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function setup(){
 canvas=createCanvas(640,420);
 canvas.center();
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status : Object Detecting...";
 }
 img="";
+status="";
+
 function preload(){
 img=loadImage('dog_cat.jpg');
 }
@@ -13,4 +17,24 @@ text("Dog",150,50);
 noFill();
 stroke('#FF0000');
 rect(50,60,520,350);
+
+fill('#FF0000');
+text("Cat",370,50);
+noFill();
+stroke('#FF0000');
+rect(300,70,250,330);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+console.log(error);
+    }else{
+        console.log(results);
+    }
 }
